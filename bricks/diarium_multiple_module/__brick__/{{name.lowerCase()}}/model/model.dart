a7d06614b601f9e3bb9b73de013775b345e77bc3
hanhan wijaya <hanhanwijaya@gmail.com>
2023-08-21 15:26:51 +0700

    feat (constants) : api and app constants

--- bricks/diarium_multiple_module/__brick__/{{name.lowerCase()}}/model/model.dart
+++ bricks/diarium_multiple_module/__brick__/{{name.lowerCase()}}/model/model.dart
@@ -1,1 +1,2 @@
 export '{{name.lowerCase()}}_model.dart';
+export '{{name.lowerCase()}}2_model.dart';
--- bricks/diarium_multiple_module/__brick__/{{name.lowerCase()}}/page/widget/widget.dart
+++ bricks/diarium_multiple_module/__brick__/{{name.lowerCase()}}/page/widget/widget.dart
@@ -1,1 +1,2 @@
 export '{{name.lowerCase()}}_widget.dart';
+export '{{name.lowerCase()}}2_widget.dart';
